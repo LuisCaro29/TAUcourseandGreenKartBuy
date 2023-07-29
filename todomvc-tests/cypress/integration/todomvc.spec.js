0c3b0de535f514401643241bc85bbfbf2425b3a6
Luis Caro <lcaro@ri.gob.do>
2023-07-29 18:43:26 -0400

    Subiendo todas las carpetas y archivos del curso realizado

---
+++ todomvc-tests/cypress/integration/todomvc.spec.js
@@ -0,0 +1,28 @@
+/// <reference types ="cypress" />
+  describe ('todo actions', () =>{
+    beforeEach(()  =>{
+        cy.visit(`http://todomvc-app-for-testing.surge.sh/`)
+        cy.url().should('eq','http://todomvc-app-for-testing.surge.sh/')
+        cy.get('.new-todo').type("Clean Room {enter}")
+    })
+  it(`Should add a new todo  to the list`, ()  => {
+   
+    cy.get('label').should('have.text', 'Clean Room')
+    cy.get ('.toggle').should('not.be.checked')
+
+  })
+  it('Should  mark  a todo as completed', ()  => {
+
+    cy.get('.toggle').click()
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+  
+    
+  })
+  it('Should  clear completed todos', ()  => {
+    cy.get('.toggle').click()
+    cy.contains('Clear completed').click()
+    cy.get('.todo-list').should('not.have.descendants', ('li'))
+    
+  })
+
+})
